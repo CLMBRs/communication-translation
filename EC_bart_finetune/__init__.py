be3b7133061d34f84b4738c5ca2c718754301b0c
C.M. Downey <cmdowney88@gmail.com>
2021-02-25 17:01:04 -0800

    Fix typo

--- EC_bart_finetune/__init__.py
+++ EC_bart_finetune/__init__.py
@@ -3,5 +3,5 @@ Package to train language models using an Emergent Communication reference game
 """
 
 from . import (
-    agents, dataloader, modeling_bart, playgroun, util
+    agents, dataloader, modeling_bart, playground, util
 )
