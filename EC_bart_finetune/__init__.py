072c043d3bd07ee8eca3417ed58d22eef30145c0
LeoLiu1998 <zeyuliu2@uw.edu>
2021-03-03 08:43:11 -0800

    delete deprecated folders from remote

--- EC_bart_finetune/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Package to train language models using an Emergent Communication reference game
-"""
-
-from . import (
-    agents, bart_models, dataloader, forward, gumbel_utils, modeling_bart, util
-)
