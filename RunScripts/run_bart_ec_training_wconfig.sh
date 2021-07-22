ff454bdabe17bfdc43c8eecd10ac176bfea6260a
C.M. Downey <cmdowney88@gmail.com>
2021-07-22 09:27:16 -0700

    Remove outdated configs and shell scripts

--- RunScripts/run_bart_ec_training_wconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# python -u EC_finetune/playground.py --config Configs/parameter_sample.yml
-# python -u EC_finetune/playground.py --config Configs/mbart_first_ex.yml
-python -u EC_finetune/playground.py --config Configs/mbart_wcons.yml
