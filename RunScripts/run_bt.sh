531aa7a895460437220a0b4bfeaf1b0aec06929f
C.M. Downey <cmdowney88@gmail.com>
2021-07-28 10:55:41 -0700

    Change translation printing to use a little less space

--- RunScripts/run_bt.sh
+++ RunScripts/run_bt.sh
@@ -2,6 +2,7 @@
 source activate unmt
 CONFIG_FILE=$1
 
-python BackTranslation/backtranslate.py \
+python -u BackTranslation/backtranslate.py \
     --config Configs/$CONFIG_FILE \
-    --threshold 0.99
+    --threshold 0.85 \
+    --val_metric_name sacrebleu
