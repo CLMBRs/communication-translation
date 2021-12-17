4417e909e07a130962168f438f2345c95efe21e1
C.M. Downey <cmdowney88@gmail.com>
2021-12-17 13:56:38 -0800

    Update pipeline scripts

--- RunScripts/en-zh_pipeline.sh
+++ RunScripts/en-zh_pipeline.sh
@@ -8,8 +8,12 @@ cp -r Output/en-zh_pipeline/captions/* Output/en-zh_pipeline/last
 # Initial EC/BT round does not have weight drift loss
 python -u EC_finetune/__main__.py --config Configs/en-zh_ec.yml
 python -u BackTranslation/backtranslate.py --config Configs/en-zh_bt_secondary.yml --seed_override 2
+mkdir Output/en-zh_pipeline/checkpoint_1
+cp -r Output/en-zh_pipeline/last/* Output/en-zh_pipeline/checkpoint_1
 for i in {2..12}
 do
-   python -u EC_finetune/__main__.py --config Configs/en-zh_ec.yml --seed_override $((i + 1)) --drift_loss_override
+   python -u EC_finetune/__main__.py --config Configs/en-zh_ec.yml --seed_override $((i + 1))
    python -u BackTranslation/backtranslate.py --config Configs/en-zh_bt_secondary.yml --seed_override $((i + 1))
+   mkdir Output/en-zh_pipeline/checkpoint_${i}
+   cp -r Output/en-zh_pipeline/last/* Output/en-zh_pipeline/checkpoint_${i}
 done
