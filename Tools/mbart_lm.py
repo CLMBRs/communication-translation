508fa8cbbc09b85bf1917f169619bf659d2cf279
C.M. Downey <cmdowney88@gmail.com>
2022-02-21 12:19:16 -0800

    Change lm eval script to use MBartForCausalLanguageModeling

--- Tools/mbart_lm.py
+++ Tools/mbart_lm.py
@@ -3,11 +3,9 @@ import argparse
 import torch
 import torch.nn.functional as F
 
-import numpy as np
 from transformers import MBartTokenizer
 
-from EC_finetune.modelings.modeling_mbart import MBartForConditionalGeneration
-from EC_finetune.modelings.modeling_bart import _prepare_bart_decoder_inputs
+from EC_finetune.modelings.modeling_mbart import MBartForCausalLanguageModeling
 
 def main():
     parser = argparse.ArgumentParser(
@@ -32,40 +30,17 @@ def main():
         return_tensors='pt'
     )
 
-    mbart = MBartForConditionalGeneration.from_pretrained(
+    language_model = MBartForCausalLanguageModeling.from_pretrained(
         args.model_path
     ).to(device)
-    mbart.eval()
+    language_model.eval()
 
-    language_model = mbart.model.decoder
-    embedding = mbart.model.shared
-    output_bias = mbart.final_logits_bias
+    lm_targets = batch['input_ids']
+    lm_output = language_model(**batch).transpose(1, 2)
 
-    lm_ids = batch['input_ids']
-    decoder_input_ids, decoder_padding_mask, causal_mask = _prepare_bart_decoder_inputs(
-        config=mbart.config,
-        input_ids=lm_ids,
-        causal_mask_dtype=embedding.weight.dtype
-    )
-    lm_targets = decoder_input_ids[:,1:].to(device)
-    decoder_input_ids = decoder_input_ids[:,:-1].to(device)
-    decoder_padding_mask = decoder_padding_mask[:,:-1].to(device)
-    causal_mask = causal_mask[:-1,:-1].to(device)
-    lm_output = language_model(
-        input_ids=decoder_input_ids,
-        encoder_hidden_states=None,
-        encoder_padding_mask=None,
-        decoder_padding_mask=decoder_padding_mask,
-        decoder_causal_mask=causal_mask
-    )
-    lm_logits = F.linear(
-        lm_output.last_hidden_state,
-        embedding.weight,
-        bias=output_bias.to(device)
-    )
     for idx, line in enumerate(lines):
         lm_loss = F.cross_entropy(
-            lm_logits[idx],
+            lm_output[idx],
             lm_targets[idx],
             ignore_index=padding_index
         )
