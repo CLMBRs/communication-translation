bee5c1af4806a125b75e2af87d2022f6bf3f0e4f
XuhuiZhou <zhouxuhui2018@gmail.com>
2022-07-15 12:44:33 -0700

    change vision feats module for adoptation into the pipeline

--- Vision_feats/generate_feats.py
+++ Vision_feats/generate_feats.py
@@ -4,6 +4,7 @@ import os
 import csv
 import argparse
 from pyexpat import model
+from turtle import forward
 import pandas as pd
 import numpy as np
 from PIL import Image
@@ -81,6 +82,7 @@ class MyResNet50(nn.Module):
         model_output = self.conv_layers(x)
         return model_output
 
+
 class MyBeit(nn.Module):
     def __init__(self):
         """Initialize network layers.
@@ -98,44 +100,58 @@ class MyBeit(nn.Module):
         model_output = model_output.last_hidden_state
         return model_output
 
-BeitFeat = BeitFeatureExtractor.from_pretrained("microsoft/beit-large-patch16-224") 
-transforms_dict = {
-    'resnet': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-             std=[0.229, 0.224, 0.225]), #Imagenet pretraining default normalization.
-        ]),
-    # 'beit': transforms.Compose([ #beit's feature extractor would do rest of the job
-    #     transforms.Resize(256, 256),
-    #     transforms.ToTensor(),
-    #     ]),
-      'beit': lambda x: torch.from_numpy(BeitFeat(x)['pixel_values'][0]) 
-}
 
-models_dict = {
-    'resnet': MyResNet50,
-    'beit': MyBeit, 
-}
+class VisionEncode(nn.Module):
+    def __init__(self, args):
+        """A vision encoder for processing images on the fly
+        """
+        self.feature_extractor = BeitFeatureExtractor.from_pretrained("microsoft/beit-large-patch16-224")
+        self.transforms_dict = {
+            'resnet': transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225]), #Imagenet pretraining default normalization.
+                ]),
+            # 'beit': transforms.Compose([ #beit's feature extractor would do rest of the job
+            #     transforms.Resize(256, 256),
+            #     transforms.ToTensor(),
+            #     ]),
+            'beit': lambda x: torch.from_numpy(self.feature_extractor(x)['pixel_values'][0]) 
+        }
+        self.models_dict = {
+            'resnet': MyResNet50,
+            'beit': MyBeit, 
+        }
+        # Load model
+        self.model = self.models_dict[args.model]()
+        self.args = args 
 
-def downsample(feats, args):
-    """
-    reduce the number of tokens in the image feature.
-    """
-    batch_size = feats.shape[0]
-    s_feats = feats[:, 0].unsqueeze(1)
-    o_feats = feats[:, 1:]
-    num_patch = int(np.sqrt((o_feats.shape[1])))
-    embed_size = s_feats.shape[-1]
-    o_feats = o_feats.transpose(2,1)
-    o_feats = o_feats.reshape(batch_size, embed_size, num_patch, num_patch)
-    m = nn.AvgPool2d(args.kernel_size, stride=args.kernel_size)
-    o_feats = m(o_feats) # (batch_size, embed_size, new_num_patch, new_num_patch)
-    o_feats = o_feats.reshape(batch_size, embed_size, -1)
-    o_feats = o_feats.transpose(2, 1)
-    feats = torch.cat([s_feats, o_feats], dim=1)
-    return feats
+    def downsample(self, feats):
+        """
+        reduce the number of tokens in the image feature.
+        """
+        batch_size = feats.shape[0]
+        s_feats = feats[:, 0].unsqueeze(1)
+        o_feats = feats[:, 1:]
+        num_patch = int(np.sqrt((o_feats.shape[1])))
+        embed_size = s_feats.shape[-1]
+        o_feats = o_feats.transpose(2,1)
+        o_feats = o_feats.reshape(batch_size, embed_size, num_patch, num_patch)
+        m = nn.AvgPool2d(self.args.kernel_size, stride=self.args.kernel_size)
+        o_feats = m(o_feats) # (batch_size, embed_size, new_num_patch, new_num_patch)
+        o_feats = o_feats.reshape(batch_size, embed_size, -1)
+        o_feats = o_feats.transpose(2, 1)
+        feats = torch.cat([s_feats, o_feats], dim=1)
+        return feats
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        feats = self.model(x)
+        feats = feats.squeeze()
+        if self.args.do_downsample:
+            feats = self.downsample(feats, self.args)
+        return feats
 
 
 def main():
@@ -157,7 +173,7 @@ def main():
     loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
 
     # Load model
-    model = models_dict[args.model]() 
+    model = VisionEncode(args) 
     model.to(device)
     model.eval()
 
@@ -166,9 +182,6 @@ def main():
         for x in tqdm(loader):
             x = x.to(device)
             feats = model(x)
-            feats = feats.squeeze()
-            if args.do_downsample:
-                feats = downsample(feats, args)
             feats_list.append(feats.cpu())
         
     all_feats = torch.cat(feats_list, dim=0)
