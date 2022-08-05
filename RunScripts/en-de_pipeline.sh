e1df7ac85b2694b10371d788cc60b0ddb958fa10
XuhuiZhou <zhouxuhui2018@gmail.com>
2022-08-05 13:52:49 -0700

    run clip large experiments

--- RunScripts/en-de_pipeline.sh
+++ RunScripts/en-de_pipeline.sh
@@ -7,14 +7,14 @@ CAPTIONS_CONFIG=$3
 EC_CONFIG=$4
 BT_SECONDARY_CONFIG=$5
 
-# Do initial (short) backtranslation
+# # Do initial (short) backtranslation
 # python -u BackTranslation/backtranslate.py --config Configs/${BT_INIT_CONFIG}.yml
 
-# # Do caption training
-# python -u -m EC_finetune --config Configs/${CAPTIONS_CONFIG}.yml
+# Do caption training
+python -u -m EC_finetune --config Configs/${CAPTIONS_CONFIG}.yml
 
-# # Do EC
-# python -u -m EC_finetune --config Configs/${EC_CONFIG}.yml
+# Do EC
+python -u -m EC_finetune --config Configs/${EC_CONFIG}.yml
 
 # cp ${OUTPUT_DIR}/bt_init/de-en.en.val ${OUTPUT_DIR}
 # cp ${OUTPUT_DIR}/bt_init/de-en.de.val ${OUTPUT_DIR}
@@ -30,16 +30,16 @@ BT_SECONDARY_CONFIG=$5
 # de2en=$(./Tools/bleu.sh ${OUTPUT_DIR}/de-en.de.val.en ${OUTPUT_DIR}/de-en.en.val 13a)
 # echo 'en to de score: '"$en2de"'; de to en score: '"$de2en"
 
-# # Do rest of backtranslation
-# python -u BackTranslation/backtranslate.py --config Configs/${BT_SECONDARY_CONFIG}.yml --seed_override 2
+# Do rest of backtranslation
+python -u BackTranslation/backtranslate.py --config Configs/${BT_SECONDARY_CONFIG}.yml --seed_override 2
 
 # Do test
 # cp /projects/unmt/communication-translation/Data/translation_references/de-en.* ${OUTPUT_DIR}
-# python -u BackTranslation/translate.py --config Configs/en2de_translate.yml \
-#     --output_dir ${OUTPUT_DIR} \
-#     --model_path ${OUTPUT_DIR}/bt_sec_clip/best_bleu
-# python -u BackTranslation/translate.py --config Configs/de2en_translate.yml \
-#     --output_dir ${OUTPUT_DIR} \
-#     --model_path ${OUTPUT_DIR}/bt_sec_clip/best_bleu
+python -u BackTranslation/translate.py --config Configs/en2de_translate.yml \
+    --output_dir ${OUTPUT_DIR} \
+    --model_path ${OUTPUT_DIR}/bt_sec_clipL/best_bleu
+python -u BackTranslation/translate.py --config Configs/de2en_translate.yml \
+    --output_dir ${OUTPUT_DIR} \
+    --model_path ${OUTPUT_DIR}/bt_sec_clipL/best_bleu
 ./Tools/bleu.sh ${OUTPUT_DIR}/de-en.en.test.de ${OUTPUT_DIR}/de-en.de.test 13a
 ./Tools/bleu.sh ${OUTPUT_DIR}/de-en.de.test.en ${OUTPUT_DIR}/de-en.en.test 13a
