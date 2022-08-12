ccc5137a23b74542e9ac8030d0027c05632bc5c4
anthonymirand <anthonypmirand@gmail.com>
2022-08-11 21:36:23 -0700

    Update testing RunScripts to use local golden translation files

--- RunScripts/test_en-de_pipeline.sh
+++ RunScripts/test_en-de_pipeline.sh
@@ -6,22 +6,12 @@ MODEL_CONFIG=$2
 
 # Example default run: test_en-de_pipeline.sh Output/en-de_pipeline Output/en-de_pipeline/best_bleu
 
-# Move golden translation files to the local folder for ease of use
-cp -vn DataLink/translation_references/de-en.de.test ${OUTPUT_DIR}
-cp -vn DataLink/translation_references/de-en.en.test ${OUTPUT_DIR}
-
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-de_pipeline: Calculating en->de test translation scores"
-python -u BackTranslation/translate.py \
-    --config Configs/test_en2de_translate.yml \
-    --output_dir ${OUTPUT_DIR} \
-    --model_path ${MODEL_CONFIG}
-en2de="$(./Tools/bleu.sh ${OUTPUT_DIR}/de-en.en.test.de ${OUTPUT_DIR}/de-en.de.test 13a)"
+python -u BackTranslation/translate.py --config Configs/test_en2de_translate.yml
+en2de="$(./Tools/bleu.sh ${OUTPUT_DIR}/translation_results/de-en.en.test.de Data/translation_references/de-en.de.test 13a)"
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-de_pipeline: en->de test bleu: $en2de"
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-de_pipeline: Calculating de->en test translation scores"
-python -u BackTranslation/translate.py \
-    --config Configs/test_de2en_translate.yml \
-    --output_dir ${OUTPUT_DIR} \
-    --model_path ${MODEL_CONFIG}
-de2en="$(./Tools/bleu.sh ${OUTPUT_DIR}/de-en.de.test.en ${OUTPUT_DIR}/de-en.en.test 13a)"
+python -u BackTranslation/translate.py --config Configs/test_de2en_translate.yml
+de2en="$(./Tools/bleu.sh ${OUTPUT_DIR}/translation_results/de-en.de.test.en Data/translation_references/de-en.en.test 13a)"
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-de_pipeline: de->en test bleu: $de2en"
--- RunScripts/test_en-zh_pipeline.sh
+++ RunScripts/test_en-zh_pipeline.sh
@@ -6,22 +6,12 @@ MODEL_CONFIG=$2
 
 # Example default run: test_en-zh_pipeline.sh Output/en-zh_pipeline Output/en-zh_pipeline/best_bleu
 
-# Move golden translation files to the local folder for ease of use
-cp -vn DataLink/translation_references/zh-en.zh.test ${OUTPUT_DIR}
-cp -vn DataLink/translation_references/zh-en.en.test ${OUTPUT_DIR}
-
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-zh_pipeline: Calculating en->zh test translation scores"
-python -u BackTranslation/translate.py \
-    --config Configs/test_en2zh_translate.yml \
-    --output_dir ${OUTPUT_DIR} \
-    --model_path ${MODEL_CONFIG}
-en2zh="$(./Tools/bleu.sh ${OUTPUT_DIR}/zh-en.en.test.zh ${OUTPUT_DIR}/zh-en.zh.test 13a)"
+python -u BackTranslation/translate.py --config Configs/test_en2zh_translate.yml
+en2zh="$(./Tools/bleu.sh ${OUTPUT_DIR}/translation_results/zh-en.en.test.zh Data/translation_references/zh-en.zh.test 13a)"
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-zh_pipeline: en->zh test bleu: $en2zh"
 
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-zh_pipeline: Calculating zh->en test translation scores"
-python -u BackTranslation/translate.py \
-    --config Configs/test_zh2en_translate.yml \
-    --output_dir ${OUTPUT_DIR} \
-    --model_path ${MODEL_CONFIG}
-zh2en="$(./Tools/bleu.sh ${OUTPUT_DIR}/zh-en.zh.test.en ${OUTPUT_DIR}/zh-en.en.test 13a)"
+python -u BackTranslation/translate.py --config Configs/test_zh2en_translate.yml
+zh2en="$(./Tools/bleu.sh ${OUTPUT_DIR}/translation_results/zh-en.zh.test.en Data/translation_references/zh-en.en.test 13a)"
 echo "$(date +'%Y-%m-%d %H:%M:%S') test_en-zh_pipeline: zh->en test bleu: $zh2en"
