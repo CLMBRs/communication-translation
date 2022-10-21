f10484ffe1c34a39626556b0c2e24a0cb20a56b8
leo-liuzy <zeyuliu2@cs.washington.edu>
2022-10-21 16:55:25 -0700

    update cloud setup script

--- Tools/cloud_setup.sh
+++ Tools/cloud_setup.sh
@@ -53,4 +53,5 @@ conda init
 echo "Done."
 # For some reason, install from bash script doesn't work
 echo "Note: 'pip install -r requirements.txt' needs to be run separately"
+echo "Note: to install torch, use 'pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html'"
 echo
