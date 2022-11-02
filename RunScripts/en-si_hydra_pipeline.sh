751ec9dc8ab0a41237c84c6ba7c54b049a93810a
Leo <zeyuliu2@cs.washington.edu>
2022-11-02 16:33:35 +0000

    fix a bug in second call to bt

--- RunScripts/en-si_hydra_pipeline.sh
+++ RunScripts/en-si_hydra_pipeline.sh
@@ -20,12 +20,12 @@ BT_SECONDARY_CONFIG=bt_secondary
 
 # Do initial (short) backtranslation
 INIT_BT_OUT_DIR=bt_init
-python -u BackTranslation/backtranslate.py \
-    +backtranslate=${BT_INIT_CONFIG} \
-    backtranslate/data=${LANG} \
-    backtranslate.train_eval.seed=${SEED} \
-    backtranslate.train_eval.val_dataset_script=BackTranslation/flores/flores.py \ # this is important
-    backtranslate.output_dir=${OUTPUT_ROOT_DIR}/${OUTPUT_BASE_DIR}/${INIT_BT_OUT_DIR}/
+# python -u BackTranslation/backtranslate.py \
+#     +backtranslate=${BT_INIT_CONFIG} \
+#     backtranslate/data=${LANG} \
+#     backtranslate.train_eval.seed=${SEED} \
+#     backtranslate.train_eval.val_dataset_script=BackTranslation/flores/flores.py \ # this is important
+#     backtranslate.output_dir=${OUTPUT_ROOT_DIR}/${OUTPUT_BASE_DIR}/${INIT_BT_OUT_DIR}/
 
 
 # Do caption training
@@ -81,6 +81,7 @@ python -u BackTranslation/backtranslate.py \
     +backtranslate=${BT_SECONDARY_CONFIG} \
     backtranslate/data=${LANG} \
     backtranslate.train_eval.seed=$((SEED + 7)) \
+    backtranslate.train_eval.val_dataset_script=BackTranslation/flores/flores.py \
     backtranslate.model_path=${OUTPUT_ROOT_DIR}/${OUTPUT_BASE_DIR}/${EC_OUT_DIR}   \
     backtranslate.output_dir=${OUTPUT_DIR}
 
