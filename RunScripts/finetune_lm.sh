1bb4d4855af2ebc3db1e499d18acb3c26fbf722e
leo-liuzy <zeyuliu2@cs.washington.edu>
2022-12-01 17:57:48 -0800

    Update runscripts and Configs and condor script

--- RunScripts/finetune_lm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source activate unmt
-
-python Tools/finetune_lm.py --config Configs/train_lm.yml
--- RunScripts/run_ec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source activate unmt
-CONFIG_FILE=$1
-
-python -u -m EC_finetune --config Configs/$CONFIG_FILE
