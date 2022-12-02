e1f954017b4a455e6eec27833eaa3f6298059b01
leo-liuzy <zeyuliu2@cs.washington.edu>
2022-12-01 17:55:59 -0800

    Fix a bug in t2i caption config

--- t2i_en-ne_clipL+transformer_seed1_all15.cmd
+++ t2i_en-ne_clipL+transformer_seed1_all15.cmd
@@ -1,9 +1,9 @@
 executable = RunScripts/t2i_pipeline_all15.sh
 arguments = "en-ne clipL transformer 1"
 getenv = true
-output = log/t2i_en-ne_clipL+transformer_seed1.out
-error = log/t2i_en-ne_clipL+transformer_seed1.err
-log = log/t2i_en-ne_clipL+transformer_seed1.log
+output = log/t2i_en-ne_clipL+transformer_seed1_all15.out
+error = log/t2i_en-ne_clipL+transformer_seed1_all15.err
+log = log/t2i_en-ne_clipL+transformer_seed1_all15.log
 Requirements = (( machine == "patas-gn3.ling.washington.edu"  ))
 request_GPUs = 1
 transfer_executable = false
