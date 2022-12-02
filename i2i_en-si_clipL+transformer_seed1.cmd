51f8093cf92c8decd21565c839cf1e600957c4fc
leo-liuzy <zeyuliu2@cs.washington.edu>
2022-12-02 15:10:31 -0800

    add run scrip for i2i all language pair

---
+++ i2i_en-si_clipL+transformer_seed1.cmd
@@ -0,0 +1,12 @@
+executable = RunScripts/i2i_pipeline.sh
+arguments = "en-si clipL transformer 1"
+getenv = true
+output = log/i2i_en-si_clipL+transformer_seed1.out
+error = log/i2i_en-si_clipL+transformer_seed1.err
+log = log/i2i_en-si_clipL+transformer_seed1.log
+Requirements = (( machine == "patas-gn3.ling.washington.edu"  ))
+request_GPUs = 1
+transfer_executable = false
+notification = always
++Research = true
+queue
---
+++ i2i_en-si_clipL+transformer_seed3.cmd
@@ -0,0 +1,12 @@
+executable = RunScripts/i2i_pipeline.sh
+arguments = "en-si clipL transformer 3"
+getenv = true
+output = log/i2i_en-si_clipL+transformer_seed3.out
+error = log/i2i_en-si_clipL+transformer_seed3.err
+log = log/i2i_en-si_clipL+transformer_seed3.log
+Requirements = (( machine == "patas-gn3.ling.washington.edu"  ))
+request_GPUs = 1
+transfer_executable = false
+notification = always
++Research = true
+queue
